2ad720162e855925bb7053ebf4d0ac70811b1165
mkurniarmadhan <mkurniarmadhan@gmail.com>
2025-10-09 11:33:34 +0700

    fix :admin panel layout dan logout button

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,8 +1,27 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
+
+<x-layouts.app>
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <div class="section-header-back">
+                    <a href="{{ route('posts.index') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+                </div>
+                <h1>Profile</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Posts</a></div>
+                    <div class="breadcrumb-item">Profile</div>
+                </div>
+            </div>
+
+            <div class="section-body">
+                <h2 class="section-title">Profile</h2>
+
+
+
+
+                  <div class="row justify-content-center">
         <div class="col-md-8">
             <h2 class="mb-5">{{ __('Profile') }}</h2>
             <div class="mb-4">
@@ -16,5 +35,7 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+            </div>
+        </section>
+    </div>
+</x-layouts.app>
